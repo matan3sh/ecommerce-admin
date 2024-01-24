76596c3a852bc2142374054297390c26ea302137
Matan <matan3sh@gmail.com>
2024-01-24 21:24:14 +0200

    Implement onSubmit functionality in settings-form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,9 +14,12 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Store } from "@prisma/client"
+import axios from "axios"
 import { Trash } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
 import * as z from "zod"
 
 interface SettingsFormProps {
@@ -30,6 +33,9 @@ const formSchema = z.object({
 type SettingFormValues = z.infer<typeof formSchema>
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams()
+  const router = useRouter()
+
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
 
@@ -39,7 +45,16 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   })
 
   const onSubmit = async (data: SettingFormValues) => {
-    console.log(data)
+    try {
+      setLoading(true)
+      await axios.patch(`/api/stores/${params.storeId}`, data)
+      router.refresh()
+      toast.success("Store updated.")
+    } catch (error) {
+      toast.error("Something went wrong.")
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
