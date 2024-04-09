40471e475472f04c1cb89806d02d9e38f74ec03d
Matan <matan3sh@gmail.com>
2024-04-09 19:21:35 +0300

    API Route Copy

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,29 +1,35 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Badge, BadgeProps } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { Copy, Server } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Badge, BadgeProps } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Copy, Server } from 'lucide-react'
+import toast from 'react-hot-toast'
 
 interface ApiAlertProps {
   title: string
   description: string
-  variant: "public" | "admin"
+  variant: 'public' | 'admin'
 }
 
-const textMap: Record<ApiAlertProps["variant"], string> = {
-  public: "Public",
-  admin: "admin",
+const textMap: Record<ApiAlertProps['variant'], string> = {
+  public: 'Public',
+  admin: 'admin',
 }
 
-const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
-  public: "secondary",
-  admin: "destructive",
+const variantMap: Record<ApiAlertProps['variant'], BadgeProps['variant']> = {
+  public: 'secondary',
+  admin: 'destructive',
 }
 
 export const ApiAlert: React.FC<ApiAlertProps> = ({
   title,
   description,
-  variant = "public",
+  variant = 'public',
 }) => {
+  const onCopy = () => {
+    navigator.clipboard.writeText(description)
+    toast.success('API Route copied to clipboard.')
+  }
+
   return (
     <Alert>
       <Server className="h-4 w-4" />
@@ -34,7 +40,7 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
           {description}
         </code>
-        <Button variant={"outline"} size="icon" onClick={() => {}}>
+        <Button variant={'outline'} size="icon" onClick={onCopy}>
           <Copy className="h-4 w-4" />
         </Button>
       </AlertDescription>
