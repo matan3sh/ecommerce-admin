be351e2d90f2824e827681055ca2013726dc4c94
Matan <matan3sh@gmail.com>
2024-04-13 18:57:42 +0300

    Start Implementing CellAction Component

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import CellAction from '@/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action'
 import { ColumnDef } from '@tanstack/react-table'
 
 export type BillboardColumn = {
@@ -16,5 +17,11 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   {
     accessorKey: 'createdAt',
     header: 'Date',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return <CellAction data={row.original} />
+    },
   },
 ]
