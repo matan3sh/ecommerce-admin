ab8f94dfa6d4837947ee1ab4c9a7e521b51827db
Matan <matan3sh@gmail.com>
2024-04-14 20:04:24 +0300

    Implement UI Template for Category Entity

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import {
+  CategoryColumn,
+  columns,
+} from '@/app/(dashboard)/[storeId]/(routes)/categories/components/columns'
+import ApiList from '@/components/ui/api-list'
+import { Button } from '@/components/ui/button'
+import { DataTable } from '@/components/ui/data-table'
+import { Heading } from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import { Plus } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+
+interface CategoryClientProps {
+  data: CategoryColumn[]
+}
+
+const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter()
+  const params = useParams()
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API calls for Categories" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  )
+}
+
+export default CategoryClient
