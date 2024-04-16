8a19528d63c4a3a48192d248b1dc05a54ebbe091
Matan <matan3sh@gmail.com>
2024-04-16 08:34:37 +0300

    Cell Action Size Adjusments

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -29,19 +29,17 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id)
-    toast.success('Billboard Id copied to clipboard.')
+    toast.success('Size Id copied to clipboard.')
   }
 
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`)
       router.refresh()
-      toast.success('Billboard deleted.')
+      toast.success('Size deleted.')
     } catch (error: any) {
-      toast.error(
-        'Make sure you removed all categories using this billboard first.'
-      )
+      toast.error('Make sure you removed all products using this size first.')
     } finally {
       setLoading(false)
       setOpen(false)
@@ -70,9 +68,7 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
